bccd5297b993530b126760f9675ac85fbfa45ccb
Shoukugeki02 <karn@eng.cmu.ac.th>
2019-12-27 18:36:45 +0700

    v1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,5 +1,16 @@
-#include (iostream)
+#include <iostream>
+using namespace std;
+int main()
 {
-    
+float N = 2;
+float hanaharumi = 0;
+ 
+ while (N <= 69){
+ 	hanaharumi = hanaharumi+(1/N);
+    N = N+1;
 
+ }  
+ cout << hanaharumi; 
+ return 0;
 }
+
