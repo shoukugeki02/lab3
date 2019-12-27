08302f2b824444c1d9f275cb13896349fee76f69
Shoukugeki02 <karn@eng.cmu.ac.th>
2019-12-27 19:11:02 +0700

    v2

---
+++ lab3_5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+float N = 6;
+float Total = 0;
+ 
+ while (N <= 19){
+ 	Total = Total+(1/N);
+    N = N+1;
+
+ }  
+ cout << Total; 
+ return 0;
+}
+
+
+
+
+    
